79ac9dd230d0560fb221b837c1f44f0759f1a3c2
smokovsky <chenshang920@hotmail.com>
2018-07-11 20:39:10 +1000

    add features

--- src/app/gameInit.js
+++ src/app/gameInit.js
@@ -1,15 +1,19 @@
 import iconNameList from './iconNameList.js';
 
+
+let internalList = [...iconNameList];
+
 const iconSelect = () => {
-  let index = Math.round(Math.random() * (iconNameList.length - 1));
-  let iconName = iconNameList[index];
-  iconNameList.splice(iconNameList.indexOf(iconName), 1);
+  let index = Math.round(Math.random() * (internalList.length - 1));
+  let iconName = internalList[index];
+  internalList.splice(internalList.indexOf(iconName), 1);
   return iconName;
 };
 
 let iconList = [];
 
 const dispIconListGen = (size) => {
+  internalList = [...iconNameList];
   size /= 2;
   for(let i = 0; i < size; i++){
     iconList.push(iconSelect());
@@ -19,6 +23,7 @@ const dispIconListGen = (size) => {
 
 const dispIconSelect = () => {
   let index = Math.round(Math.random() * (iconList.length - 1));
+  //console.log(iconList);
   let iconName = iconList[index];
   iconList.splice(iconList.indexOf(iconName), 1);
   return iconName;
--- src/app/index.js
+++ src/app/index.js
@@ -1,9 +1,11 @@
 import $ from 'jQuery';
 import '../style/css/fontawesome.min.css';
 import '../style/css/fa-regular.min.css';
+import '../style/css/fa-solid.min.css';
 import '../style/main.css';
 import isOdd from 'is-odd';
 import {gameInit, dispIconSelect} from './gameInit';
+import durationFormatter from 'format-duration';
 
 const gameState = {
   size: 0,
@@ -31,15 +33,19 @@ const resetGame = () => {
     oFin:false,
     inProgess:false,
   });
-  //const container = $('#gameContainer');
-  $('div[data-type="game"],#score')? $('div[data-type="game"],#score').remove() :null;
+  const container = $('#gameContainer').html('');
+  $('#util').remove();
+  clearInterval(timer);
+  // $('div[data-type="game"],#score')? $('div[data-type="game"],#score').remove() :null;
 };
 
 const gameCleared = ()=>{
   const container = $('#gameContainer');
+  container.css({padding:'1px'});
   let score = gameState.score;
-  resetGame();
-  $(`<div>Congrats! You scored ${score}</div>`).css({margin:'auto auto', width:'auto',height:'100px',textAlign:'center'}).appendTo(container);
+  container.html('');
+  $(`<div>Congrats!<br>You scored: ${Math.round(gameState.score/10*3)} stars<br>Time consumed: ${durationFormatter(Date.now()-currentTime)}</div>`).css({margin:'auto auto', width:'auto',height:'100px',textAlign:'center'}).appendTo(container);
+  $('<div>Try Again</div>').css({border:'white solid 1px','borderRadius':'5px', cursor:'pointer', margin:'5px auto',width:'70px'}).click(gameSetup).appendTo(container);
 };
 
 const stepClear = () => {
@@ -48,6 +54,10 @@ const stepClear = () => {
   gameState.clicked = [];
   gameState.zFin = false;
   gameState.oFin = false;
+};
+
+const updateMoves = (moves) => {
+  $('#moves').html('Moves:'+moves);
 };
 
 function handleClick(){
@@ -61,10 +71,11 @@ function handleClick(){
     else{
       gameState.clicked.push(target);
       gameState.clickTwo = target.attr('icon-name');
-
       if(gameState.clickOne == gameState.clickTwo){
         //console.log((gameState.clicked)[0], gameState.clicked[1]);
         gameState.corrected.push(gameState.clickOne);
+        gameState.moves++;
+        updateMoves(gameState.moves);
         let cb = ()=>{
 
           let cb0 = ()=>{
@@ -95,6 +106,8 @@ function handleClick(){
         };
         tileAnimate(target, 'flip', cb);
       }else{
+        gameState.moves++;
+        updateMoves(gameState.moves);
         let cb = ()=>{
 
           let cb0 = ()=>{
@@ -117,12 +130,30 @@ function handleClick(){
         tileAnimate(target, 'flip', cb);
         gameState.score -= Math.floor(gameState.deduction/gameState.size);
         gameState.deduction = gameState.deduction >= gameState.size ? 0 : gameState.deduction+1;
-        $('#score').html(`Your Score: ${gameState.score}`);
+        updateScore(gameState.score);
+        //console.log(gameState.score, gameState.deduction);
+        //$('#score').html(`Your Score: ${gameState.score}`);
       }
     }
   }
 }
 // console.log(gameState);
+
+function updateScore(score){
+  let scoreDOM = $('#score').children();
+  score = Math.round(3*score/10);
+  //console.log(scoreDOM.children());
+  if(score == 2){
+    //window.score = scoreDOM;
+    $(scoreDOM[2]).removeClass('fas').addClass('far');
+  }
+  else if(score == 1){
+    $(scoreDOM[1]).removeClass('fas').addClass('far');
+  }
+  else if(score == 0){
+    $(scoreDOM[0]).removeClass('fas').addClass('far');
+  }
+}
 
 function tileAnimate(target, action, cb){
 
@@ -179,13 +210,22 @@ function tileAnimate(target, action, cb){
   }
 }
 
+let timer;
+
+function updateTimer(){
+  //console.log(currentTime);
+  $('#timer').html('Time elapsed: '+durationFormatter(Date.now() - currentTime));
+}
+
+let currentTime = Date.now();
+
 const gameSetup = function(){
   resetGame();
   const container = $('#gameContainer');
-  let form = $('<form id="game-setup">Please enter the level:<br /><input /><input type="submit" value="start"></input><br />[even numder, smaller than 20]</form>').appendTo(container);
+  let form = $('<form id="game-setup">Please enter the level:<br /><input /><input type="submit" value="start"></input><br />[even numder, smaller or equal to 14]</form>').appendTo(container);
   let handleSubmit = (e) => {
     e.preventDefault();
-    if(isOdd(e.target[0].value) || e.target[0].value <= 0 || e.target[0].value > 20)
+    if(isOdd(e.target[0].value) || e.target[0].value <= 0 || e.target[0].value > 14)
     {
       return false; //proper notification required
     }
@@ -195,7 +235,6 @@ const gameSetup = function(){
   };
   form.submit(handleSubmit);
 };
-
 
 const gameEntity = (size) => {
   size = Number(size);
@@ -207,26 +246,52 @@ const gameEntity = (size) => {
 
   gameInit(size * size);
 
-  let width = ((size + 1) * 10 + size * 50) + 'px';
+  let base = 5;
+
+  let width = 2 * base * size + 10 * base * size;
+
+  currentTime = Date.now();
+
+  if(width > $(window).width()){
+    base = Math.floor(base * ($(window).width()/width));
+  }
+
+  width = 2 * base * size + 10 * base * size;
+  // width = width + 'px';
+  // base = base + 'px';
 
   const container = $('#gameContainer');
-  $(`<div id="score">Your Score: ${gameState.score}</div>`).insertBefore(container);
-  container.css({width, height: width, backgroundColor:'#2979FF',color:'#fff', padding:'10px'});
+  let util = $('<div id="util">Score:</div>').css({width:width-2*base+'px',backgroundColor:'#2979FF',color:'#fff', padding:base+'px',borderRadius:'10px 10px 0 0', position:'relative' }).insertBefore(container);
+  let score = $('<div id="score" style="display:inline-block">');
+  for(let i = 0; i < 3; i++){
+    score.append('<i class="fas fa-star">');
+  }
+  score.appendTo(util);
+  let moves = $('<div id="moves">Moves:0</div>').appendTo(util);
+  let refresh = $('<div><i class="fas fa-sync"></div>').appendTo(util);
+  refresh.css({position:'absolute', right:base+'px',top:base+'px'});
 
+  refresh.click(gameSetup);
+
+  container.css({width, backgroundColor:'#2979FF',color:'#fff', padding:'0px'});
   const board = [];
 
   for(let i = 0; i < size; i++){
+    //console.log(base);
     board[i] = [];
-    let row = $('<div>').attr({'data-type':'game'}).css({width, height:'50px',margin:'10px 0px',padding:'0px 5px'});
+    let row = $('<div>').attr({'data-type':'game'}).css({width:width+'px', height:10*base + 'px',margin:'0px 0px',padding:base+'px 0px'});
     for(let j = 0; j < size; j++){
       let iconName = dispIconSelect();
-      board[i][j] = $('<div>').css({margin:'0px 5px',width:'50px',height:'50px',lineHeight:'50px', textAlign:'center',display:'inline-block', backgroundColor:'rgba(0,0,0,0.08)',borderRadius:'10%', color:'rgba(72,114,253,0.08)'}).appendTo(row);
+      board[i][j] = $('<div>').css({margin:'0px '+base+'px',width:10* base + 'px',height:10*base + 'px',lineHeight:10*base+'px', textAlign:'center',display:'inline-block', backgroundColor:'rgba(0,0,0,0.08)',borderRadius:'10%', color:'rgba(72,114,253,0.08)'}).appendTo(row);
       board[i][j].attr({'icon-name':iconName});
       board[i][j].click(handleClick);
       $('<i>').addClass('far '+'fa-'+iconName).appendTo(board[i][j]);
     }
     row.appendTo(container);
   }
+  $('<div id="timer" style="float:right">Time elapsed:00:00</div>').appendTo(container);
+  $('<div style="clear:both"></div>').appendTo(container);
+  timer = setInterval(updateTimer, 1000);
 };
 
 gameSetup();
